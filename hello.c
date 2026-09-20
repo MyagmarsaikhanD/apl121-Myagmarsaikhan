42131d1341d87ea23282d24ec5cd4263cd901157
MyagmarsaikhanD <b21fd0063@ufe.edu.mn>
2026-09-20 23:56:16 +0800

    Clean up hello.c by removing git commands
    Removed unnecessary git commands and comments.

--- hello.c
+++ hello.c
@@ -1,9 +1,3 @@
 $gcc hello.c -o hello      # compile: produce executable named hello
 $ ./hello                   # run it
 Hello, UFE!
-
-git status 
-git add .
-git commit -m "1-r doloo honog: hello.c nemev - Hello, UFE hevlene"
-
-  git push
